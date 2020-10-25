c3382cae0ba1f23596091aaafff6b067aeddd14c
Marcelg1235 <Marcelg1235@gmail.com>
2020-10-25 14:36:20 +0100

    Adding contoller to BattleshipController

--- app/controllers/BattleshipController.scala
+++ app/controllers/BattleshipController.scala
@@ -1,18 +1,21 @@
 package controllers
 
+import Battleship._
+import Battleship.controller.ControllerBaseImpl
 import javax.inject._
-import play.api.mvc._
+import play.api.mvc._;
 
 @Singleton
 class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractController(cc) {
-  // val.gameController = Battleship.controller
-  //def battleshipAsText = gamecontroller.gridToString + GameStatus.message(gameController.gameStatus)
+  val gameController = Game.controller;
+
+  def battleship = Action {
+    Ok(battleshipAsText)
+  }
 
   def about = Action {
     Ok(views.html.index())
   }
 
-  //def battleship = Action {
-  //Ok(battleshipAsText)
-  //}
+  def battleshipAsText = gameController.getGridPlayer1 + ControllerBaseImpl.GameState.message(gameController.getGameState)
 }

579a74fa52c65afc1575d48c3eeff0b8cb57a97a
Marcelg1235 <Marcelg1235@gmail.com>
2020-11-05 11:18:20 +0100

    Adding setSip.html and setPlayer.html

--- app/controllers/BattleshipController.scala
+++ app/controllers/BattleshipController.scala
@@ -1,7 +1,9 @@
 package controllers
 
+import Battleship.Game.tui.{decreaseShipNumbersToPlace, shipProcessLong}
 import Battleship._
 import Battleship.controller.ControllerBaseImpl
+import Battleship.controller.ControllerBaseImpl.{GameState, PlayerState}
 import javax.inject._
 import play.api.mvc._;
 
@@ -10,9 +12,50 @@ class BattleshipController @Inject()(cc: ControllerComponents) extends AbstractC
   val gameController = Game.controller;
 
   def battleship = Action {
-    gameController.setPlayers("Marcel")
-    gameController.setPlayers("Matthias")
-    Ok(views.html.battleship(gameController))
+    if (gameController.getGameState == GameState.IDLE) {
+      Ok(views.html.battleship(gameController))
+    }
+    Ok(views.html.setPlayer("Player 1, please add your name :-)"))
+  }
+
+  def setPlayer(name: String) = Action {
+    System.out.println("Spieler")
+    if (gameController.getGameState == GameState.PLAYERSETTING) {
+      gameController.setPlayers(name)
+    }
+    if (gameController.getGameState == GameState.SHIPSETTING) {
+      Ok(views.html.setShip(gameController))
+    } else {
+      Ok(views.html.setPlayer("Player 2, please add your name :-)"))
+    }
+  }
+
+  def setShip(ship: String) = Action {
+    System.out.println("Ships")
+    if (gameController.getGameState == GameState.SHIPSETTING) {
+      gameController.getPlayerState match {
+        case PlayerState.PLAYER_ONE => {
+          gameController.setShipSet(false)
+          shipProcessLong(ship)
+          decreaseShipNumbersToPlace(gameController.getShip, gameController.getShipSet, gameController.getShipDelete)
+          if ((gameController.getNrPlayer1()(0) + gameController.getNrPlayer1()(1) + gameController.getNrPlayer1()(2) +
+            gameController.getNrPlayer1()(3)) == 0) {
+            gameController.setPlayerState(PlayerState.PLAYER_TWO)
+          }
+        }
+        case PlayerState.PLAYER_TWO => {
+          gameController.setShipSet(false)
+          shipProcessLong(ship)
+          decreaseShipNumbersToPlace(gameController.getShip, gameController.getShipSet, gameController.getShipDelete)
+          if (gameController.getNrPlayer2()(0) + gameController.getNrPlayer2()(1) + gameController.getNrPlayer2()(2) +
+            gameController.getNrPlayer2()(3) == 0) {
+            gameController.setPlayerState(PlayerState.PLAYER_ONE)
+            gameController.setGameState(GameState.IDLE)
+          }
+        }
+      }
+    }
+    Ok(views.html.setShip(gameController))
   }
 
   def about = Action {

5c6b797ca1b2067eca0ff968db80267347937a67
Marcelg1235 <Marcelg1235@gmail.com>
2020-11-26 06:12:34 +0100

    Adding onClick function

--- public/javascripts/gamehandler.js
+++ public/javascripts/gamehandler.js
@@ -1,3 +1,19 @@
+var lastCall;
+var startisSet = false;
+var endcall;
+
+function handleShipSetClick(row, col) {
+    if (startisSet) {
+        endcall = lastCall + " " + row + " " + col
+        lastCall = null
+        startisSet = false
+        window.location = "http://localhost:9000/setShip/" + endcall
+    } else {
+        lastCall = row + " " + col
+        startisSet = true
+    }
+}
+
 function handleClick(row, col) {
-    alert(row + " " + col)
+    window.location = "http://localhost:9000/idle/" + row + " " + col
 }

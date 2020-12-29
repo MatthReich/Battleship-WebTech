39f28e09121f9cc32c99830bfc2f549d6a3dc9b1
Matthias Reichenbach <matthreich@gmail.com>
2020-12-29 11:19:25 +0100

    added navbar

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,35 @@
+<script src="main.js"></script>
 <template>
-  <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
-    <router-view/>
-  </div>
+  <v-app id="inspire">
+    <v-app-bar app color="grey darken-2" flat>
+      <v-container class="py-0 fill-height">
+        <v-btn
+            v-for="navitem in navbar"
+            :key="navitem.title"
+            :to="navitem.to"
+            text
+        >
+          {{ navitem.title }}
+        </v-btn>
+      </v-container>
+    </v-app-bar>
+
+    <v-main class="grey">
+      <router-view></router-view>
+    </v-main>
+  </v-app>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-#nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
-    }
-  }
-}
-</style>
+<script>
+export default {
+  data: () => ({
+    navbar: [
+      { title: "Home", to: "/" },
+      { title: "Start", to: "/game" },
+      { title: "Save", to: "/save" },
+      { title: "Load", to: "/load" },
+      { title: "About", to: "/about" }
+    ]
+  })
+};
+</script>
